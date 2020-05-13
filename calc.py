b47434bf8bf83b5d4ea3a6f9bf8cec02c1db4ac5
prathap <prathap@gmail.com>
2020-05-13 07:37:05 +0000

    initial commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    """Add Function"""
+    return x + y		
+
+def subtract(x,y):
+    """Add Function"""
+    return x - y
+
+def multiply(x,y):
+    """Add Function"""
+    return x * y
+
+def divide(x,y):
+    """Add Function"""
+    return x / y
